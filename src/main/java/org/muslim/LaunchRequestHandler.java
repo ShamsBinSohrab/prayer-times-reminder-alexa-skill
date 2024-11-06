f0235e2ddbb6c4d7ae0515e184303bb4f04a6600
shamsbinsohrab <aniknishan@gmail.com>
2024-11-06 19:22:03 +0100

    initial commit

---
+++ src/main/java/org/muslim/LaunchRequestHandler.java
@@ -0,0 +1,34 @@
+package org.muslim;
+
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.LaunchRequest;
+import com.amazon.ask.model.Response;
+
+import java.util.Optional;
+
+/**
+ * The request handler for {@link LaunchRequest}
+ */
+public class LaunchRequestHandler implements com.amazon.ask.dispatcher.request.handler.impl.LaunchRequestHandler {
+
+    private static final String CARD_TITLE = "Prayer Times Reminder";
+    private static final String CARD_TEXT = """
+            Welcome to the Prayer Times Reminder Skill, I can create daily prayer reminders for you.
+            Just say 'Create daily prayer time reminders'.
+            """;
+
+    @Override
+    public boolean canHandle(HandlerInput handlerInput, LaunchRequest launchRequest) {
+        return true;
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput handlerInput, LaunchRequest launchRequest) {
+        return handlerInput.getResponseBuilder()
+                .withSpeech(CARD_TEXT)
+                .withSimpleCard(CARD_TITLE, CARD_TEXT)
+                .withReprompt(CARD_TEXT)
+                .build();
+    }
+}
